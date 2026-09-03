45efc330fffdaaec5c3324eb842023c8e36182a9
Riku Nakagoshi <r.n1052289@gmail.com>
2026-09-03 20:57:56 +0900

    fix: usersテーブルから不要な2FAカラムを削除

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,9 +19,6 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->text('two_factor_secret')->nullable();
-            $table->text('two_factor_recovery_codes')->nullable();
-            $table->timestamp('two_factor_confirmed_at')->nullable();
         });
     }
 
